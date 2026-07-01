bc2eaa65d5af2cd5852176cefca5afd6fa4ff190
conymi <conipoky1347@gmail.com>
2026-07-01 15:45:29 +0900

    홈 물까치 커스터마이징 + 제목 반응형 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'mulkkachi';
+export const SITE_DESCRIPTION = '고유한 것을 찾아 이야기로 만드는 물까치';
